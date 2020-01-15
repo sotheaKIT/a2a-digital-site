fca34a190a0214cbb9e6a360967afda80e3276fa
tamsub <tam.subtam@gmail.com>
2020-01-15 16:01:46 +0700

    feat: Service Page content card

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <style src="./styles/app.css"></style>
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/A2A-digital-logo.svg">
     <HomePage msg="A2A-digital"/>
   </div>
 </template>
