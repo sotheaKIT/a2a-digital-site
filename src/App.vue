d46b53c91af24f66b5e5dfd53d76157ffe85f813
tamsub <tam.subtam@gmail.com>
2020-01-21 15:09:47 +0700

    Fix: Fixing layout and reponsive image for service page

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,23 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/A2A-digital-logo.svg"> -->
-    <HomePage msg="A2A-digital"/>
+    <!-- <HomePage></HomePage> -->
+    <ServicePage></ServicePage>
+    <AboutPage msg="A2A-digital"/>
+    
+    
   </div>
 </template>
 <script>
-import HomePage from './views/HomePage.vue'
+// import HomePage from './views/HomePage.vue'
+import AboutPage from './views/AboutPage'
+import ServicePage from './views/ServicesPage'
 export default {
   name: 'App',
   components: {
-    HomePage
+    ServicePage,
+    AboutPage
+
   }
 }
 </script>
--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,6 +1,31 @@
+<style src="../styles/about.css" scope></style>
 <template>
   <div id="about-page">
-    
+    <div class="l_header">
+      <Navigator/>
+   </div>
+   <!-- <div class="l_wrapper">
+      <div class="blog-card">
+        <div class="meta">
+          <div class="photo" style="background-image: url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg)"></div>
+        </div>
+        <div class="description">
+          <h1>Learning to Code</h1>
+          <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
+        </div>
+      </div>
+      <div class="blog-card alt">
+        <div class="meta">
+          <div class="photo" style="background-image: url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-2.jpg)"></div>
+        </div>
+        <div class="description">
+          <h1>Mastering the Language</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit.</p>
+        </div>
+      </div>
+   </div> -->
+   <!-- End -->
+  <Footer/>
   </div>
 </template>
 
@@ -8,8 +33,16 @@
 
 
 
+import Navigator from '../components/navigator/navigator.vue'
+import Footer from '../components/footer/footer.vue'
+
 export default {
   name: 'AboutPage',
+  components: {
+    Navigator,
+    Footer
+  }
 }
 </script>
+
 
