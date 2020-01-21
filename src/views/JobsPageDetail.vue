3e89c01df0eb6f7ce89a1266ad377255e163bce7
PenSereyOtdom <pensereyotdom15@kit.edu.kh>
2020-01-21 15:48:00 +0700

    refactor: Detail page responsive for Ipad, Footer bug fix

--- src/views/JobsPageDetail.vue
+++ src/views/JobsPageDetail.vue
@@ -1,21 +1,20 @@
 <template>
     <div id="jobspage_detail"> 
-        <Navigator/>
         <JobsDetailPage/>
-        <Footer/> 
+        <Footer/>
     </div>
 </template>
 
 <script>
 
-import Navigator from '../components/navigator/navigator.vue'
+// import Navigator from '../components/navigator/navigator.vue'
 import JobsDetailPage from'../components/jobsdetail-page/jobspagedetail.vue'
 import Footer from '../components/footer/footer.vue'
 
 export default {
     name: "JobsPageDetail",
     components: {
-        Navigator,
+        
         JobsDetailPage,
         Footer
     }
