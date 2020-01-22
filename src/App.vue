fd4ef54ec8e6f8a896b04c424fb86076e93fd520
Hello-world <seaklimlay1@gmail.com>
2020-01-22 15:50:25 +0700

    feat:feature/profile

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <style src="./styles/app.css"></style>
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/A2A-digital-logo.svg"> -->
+    <img alt="Vue logo" src="./assets/A2A-digital-logo.svg">
     <HomePage msg="A2A-digital"/>
   </div>
 </template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,46 +2,51 @@
   <div id="home-page">
   <Navigator/>
   <Slide/>
-  <ServicesPage/>
+  <!-- <ServicesPage/> -->
   <!-- Just show Page only Example :  <AboutPage/>  -->
-  <AboutPage/>
+  <!-- <AboutPage/>
   <ContactPage/>
   <JobsPage/>
-  <ProductsPage/>
-  <CardProduct />
-  <PartnersPage/>
+  <ProductsPage/> -->
+  <!-- <CardProduct /> -->
+  <!-- <PartnersPage/> -->
+  <ProfilePage/>
+
    <!-- End -->
-  <Footer/>
+  <!-- <Footer/> -->
   </div>
 </template>
 
 <script>
 
-import AboutPage from '../views/AboutPage'
-import ContactPage from '../views/ContactPage'
-import JobsPage  from '../views/JobsPage'
-import ProductsPage from '../views/Products'
-import ServicesPage from '../views/ServicesPage'
-import PartnersPage from '../views/PartnersPage'
-import Navigator from '../components/navigator/navigator.vue'
-import Footer from '../components/footer/footer.vue'
-import Slide from '../components/slide/slide.vue'
-import CardProduct from '../components/card-product/card-product'
+// import AboutPage from '../views/AboutPage'
+// import ContactPage from '../views/ContactPage'
+// import JobsPage  from '../views/JobsPage'
+// import ProductsPage from '../views/Products'
+// import ServicesPage from '../views/ServicesPage'
+// import PartnersPage from '../views/PartnersPage'
+// import Navigator from '../components/navigator/navigator.vue'
+// import Footer from '../components/footer/footer.vue'
+// import Slide from '../components/slide/slide.vue'
+// import CardProduct from '../components/card-product/card-product'
+import ProfilePage from '../views/ProfilePage'
 
 
 export default {
   name: 'Home',
   components: {
-    AboutPage,
-    ContactPage,
-    JobsPage,
-    ProductsPage,
-    PartnersPage,
-    ServicesPage,
-    Navigator,
-    Footer,
-    Slide,
-    CardProduct
+    // AboutPage,
+    // ContactPage,
+    // JobsPage,
+    // ProductsPage,
+    // PartnersPage,
+    // ServicesPage,
+    // Navigator,
+    // Footer,
+    // Slide,
+    // CardProduct,
+    ProfilePage
+    
   }
 }
 </script>
