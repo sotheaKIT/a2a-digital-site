21130395ea0ba410e164f8b6c90bf66f85f6e12c
Hello-world <seaklimlay1@gmail.com>
2020-01-24 10:16:17 +0700

    perf:feature/profile

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <style src="./styles/app.css"></style>
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/A2A-digital-logo.svg">
-    <HomePage msg="A2A-digital"/>
+    <ProfilePage></ProfilePage>
   </div>
 </template>
 <script>
-import HomePage from './views/HomePage.vue'
+import ProfilePage from './views/ProfilePage'
 export default {
   name: 'App',
   components: {
-    HomePage
+      ProfilePage
   }
 }
 </script>
