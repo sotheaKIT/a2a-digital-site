b858bc2531540e541c4f551fe61ef7796e49689d
tamsub <tam.subtam@gmail.com>
2020-01-25 08:04:27 +0700

    Fix: Reponsive layout of about page

--- src/components/about-carousel/about-carousel.vue
+++ src/components/about-carousel/about-carousel.vue
@@ -1,8 +1,22 @@
+
 <style src="./about-carousel.css" scoped></style>
 <template src="./about-carousel.html"></template>
 
+
 <script>
+import Slick from 'vue-slick';
 export default {
-    name : "AboutCarouse",      
+    name : "AboutCarouse",
+    components: { Slick },
+    data() {
+        return {
+            slickOptions: {
+                slidesToShow: 3,
+                // Any other options that can be got from plugin documentation
+            },
+        };
+    },   
+       
 }
+
 </script>
