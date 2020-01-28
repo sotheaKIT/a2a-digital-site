eb1fe3ee5c77f2ef9b9b800ae0c745a29f28b873
Kimheng <kimheng@Kimhengs-MacBook-Pro.local>
2020-01-28 16:52:14 +0700

    Refector: slide Navigation

--- src/components/navigator/Menu/Menu.vue
+++ src/components/navigator/Menu/Menu.vue
@@ -1,11 +1,27 @@
-<style src="./navigator.css" scope></style>
-<template src="./navigator.html"></template>
+<template>
+    <div>
+        <div ref="sideNav" class="bm-menu">
+            <nav class="bm-item-list">
+                <slot></slot>
+            </nav>
+            <span class="bm-cross-button cross-style" @click="closeMenu" :class="{ hidden: !crossIcon }">
+                <span v-for="(x, index) in 2" :key="x" class="bm-cross" :style="{ position: 'absolute', width: '3px', height: '14px',transform: index === 1 ? 'rotate(45deg)' : 'rotate(-45deg)'}">
+                </span>
+            </span>
+        </div>
+
+        <div ref="bmBurgerButton" class="bm-burger-button" @click="openMenu" :class="{ hidden: !burgerIcon }">
+            <span class="bm-burger-bars line-style" :style="{top:20 * (index * 2) + '%'}" v-for="(x, index) in 3" :key="index"></span>
+        </div>
+
+    </div>
+    
+    
+</template>
 
 <script>
- 
- 
-  export default {
-    name: 'Navigator',
+    export default {
+      name: 'menubar',
       data() {
         return {
           isSideBarOpen: false
@@ -23,7 +39,7 @@
         width: {
           type: [String],
           required: false,
-          default: '100'
+          default: '300'
         },
         disableEsc: {
           type: Boolean,
@@ -67,7 +83,7 @@
           }
           this.$nextTick(function() {
             this.$refs.sideNav.style.width = this.width
-              ? this.width + '%'
+              ? this.width + 'px'
               : '300px';
           });
         },
@@ -165,5 +181,8 @@
           }
         }
       }
-  }
+    };
 </script>
+
+
+
