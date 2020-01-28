5bfbe148aa6b4b1ddb78ac27358f05740b04c973
soem samrach <samrachlonelyman@gmail.com>
2020-01-28 16:12:39 +0700

    Product Type and Solution

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
   <div id="app">
     <!-- <Navigator></Navigator>  -->
     <!-- <HomePage></HomePage> -->
-    <JobPage></JobPage>
+    <!-- <JobPage></JobPage> -->
+    <SolutionPage/>
+
      <!-- <Footer></Footer> -->
   </div>
 </template>
@@ -11,15 +13,18 @@
 // import HomePage from './views/HomePage.vue'
 // import Navigator from './components/navigator/navigator'
 // import Footer from './components/footer/footer'
-import JobPage from './views/JobsPage'
+// import JobPage from './views/JobsPage'
+import SolutionPage from './views/SolutionPage'
+
 
 export default {
   name: 'App',
   components: {
     //  HomePage,
-    JobPage,
+    // JobPage,
     // Navigator,
     // Footer
+    SolutionPage
   }
 }
 </script>
