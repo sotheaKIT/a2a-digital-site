69e16058447d7dce0d0d0a9ecb43702d7b811dab
Kimheng <kimheng@Kimhengs-MacBook-Pro.local>
2020-02-01 11:57:16 +0700

    Refactor: link in navigation click

--- src/router.js
+++ src/router.js
@@ -65,7 +65,7 @@ export default new Router({
     {
       path: '/serviceclouddetail',
       name: 'serviceclouddetail',
-      component: ServiceCloudDetailPage
+      // component: ServiceCloudDetailPage
     },
 
     {
