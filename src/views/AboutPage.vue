86020af4c4050032b7ab569c5392a7c6222f31e8
Soem Samrach <samrachlonelyman@gmail.com>
2020-02-04 12:05:10 +0700

    feat:about Json

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,27 +1,27 @@
 <style src="../styles/about.css" scope></style>
 <template>
   <div id="about-page">
-    <AboutHero></AboutHero>
-    <AboutContentCard></AboutContentCard>
+    <!-- <AboutHero></AboutHero> -->
+    <!-- <AboutContentCard></AboutContentCard> -->
     <AboutCard></AboutCard>
-    <AboutCarousel></AboutCarousel>
+    <!-- <AboutCarousel></AboutCarousel> -->
   </div>
 </template>
 
 <script>
 
 import AboutCard from '../components/about-card/about-card.vue'
-import AboutContentCard from '../components/about-content-card/about-content-card.vue'
-import AboutHero from '../components/about-hero/about-hero.vue'
-import AboutCarousel from '../components/about-carousel/about-carousel'
+// import AboutContentCard from '../components/about-content-card/about-content-card.vue'
+// import AboutHero from '../components/about-hero/about-hero.vue'
+// import AboutCarousel from '../components/about-carousel/about-carousel'
 
 export default {
   name: 'AboutPage',
   components: {
     AboutCard,
-    AboutContentCard,
-    AboutHero,
-    AboutCarousel
+    // AboutContentCard,
+    // AboutHero,
+    // AboutCarousel
   }
 }
 </script>
