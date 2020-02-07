cd1a6d8be31875d3f374877967a96da382a3eb24
metheany <metheany.thorn@gmail..com>
2020-02-07 13:52:15 +0700

    add vue-meta

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Meta from 'vue-meta';
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+Vue.use(Meta);
 
 Vue.config.productionTip = false
 Vue.use(VueAwesomeSwiper, /* { default global options } */)
