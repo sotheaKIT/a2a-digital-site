b140f7690b0e58beb1e5e797e357a1daddbe8b8d
SEAKLIM <LaySeaklim>
2020-02-08 09:27:24 +0700

    profile page

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -8,15 +8,15 @@
         </header> 
 
         <section class="l_card__profile">
-            <div class="card_detail">
-                <div class="img_right">
-                    <img class="c_card__img" src="../assets/component/about-card/Managers/takeshi.jpg" alt="takeshi"/>
-                </div>
+            <div class="l_card_detail">
                 <div class="c_cart__text">
-                <div>
                     <h2>Takeshi Izuka</h2>
                     <h3>President / Chairman vKirirom Pte.Ltd.</h3>
-                </div>  
+                </div>
+                <div class="c_img_right">
+                    <img class="c_card__img right" src="../../public/assets/component/about-card/Managers/takeshi.jpg" alt="takeshi"/>
+                </div> 
+                <div class="c_cart__text"> 
                     <p>Mr.Takeshi graduated from a department of Science and Engineering at Waseda University, and received a Master's 
                         degree from Tokyo Institute of Technology. He established Digital Forest Inc. in 1998 while working at Andersen 
                         Consulting (Accenture now). With the successful leading of WEB Analytics Company in Japan, the Digital Forest Inc
@@ -28,13 +28,15 @@
         </section>
 
         <section class="l_card__profile">
-            <div class="card_detail">
-                <div class="img_left">
-                    <img class="c_card__img" src="../assets/component/about-card/Managers/mashami.jpg" alt="mashami"/>
-                </div>
+            <div class="l_card_detail">
                 <div class="c_cart__text text_right">
                     <h2>Yasue Mashima</h2>
-                    <h3>Manager of A2A Digital Technology Team</h3>  
+                    <h3>Manager of A2A Digital Technology Team</h3>
+                </div>
+                <div class="c_img_left">
+                    <img class="c_card__img" src="../../public/assets/component/about-card/Managers/mashami.jpg" alt="mashami"/>
+                </div>
+                <div class="c_cart__text text_right"> 
                     <p>After graduating from university, she worked for the software development company in Japan. She has a team leader 
                         experience for global systems that include development. In 2018, she joined the global general trading company 
                         as a transferred employee in charge of the finance data analyzing team. She is working A2A Digital from February 
@@ -48,13 +50,15 @@
         </div>
 
         <section class="l_card__profile">
-            <div class="card_detail">
-                <div class="img_right">
-                    <img class="c_card__img" src="../assets/component/about-card/Advisor/bekku.jpg" alt="bekku"/>
-                </div>
+            <div class="l_card_detail">
                 <div class="c_cart__text">
                     <h2>Kenzaburo Bekku</h2>
-                    <h3>Vice President of Kirirom Institute of Technology</h3>  
+                    <h3>Vice President of Kirirom Institute of Technology</h3>
+                </div>
+                <div class="c_img_right">
+                    <img class="c_card__img right" src="../../public/assets/component/about-card/Advisor/bekku.jpg" alt="bekku"/>
+                </div>
+                <div class="c_cart__text">  
                     <p>He joined Accenture in 2004. Since then, he has been in the Manufacturing and Distribution Division and the Strategy 
                         Consulting Division. He has been engaged in management consulting for 13 years. Involved in this project since 2017,
                         his responsible for corporate planning, global marketing, and launching new businesses. </p>
@@ -63,13 +67,15 @@
         </section>
 
         <section class="l_card__profile">
-            <div class="card_detail">
-                <div class="img_left">
-                    <img class="c_card__img" src="../assets/component/about-card/Advisor/bekkuwife.jpg" alt="bekkuwife"/>
-                </div>
+            <div class="l_card_detail">
                 <div class="c_cart__text text_right">
                     <h2>Mariko Sadamatsu</h2>
-                    <h3>General Manager of Corporate Planning Office A2A Town (Cambodia) Co., Ltd</h3>  
+                    <h3>General Manager of Corporate Planning Office A2A Town (Cambodia) Co., Ltd</h3>
+                </div>
+                <div class="c_img_left">
+                    <img class="c_card__img" src="../../public/assets/component/about-card/Advisor/bekkuwife.jpg" alt="bekkuwife"/>
+                </div>
+                <div class="c_cart__text text_right">      
                     <p>After graduating from university, she worked at Accenture Co., Ltd. as a number of program management and CIO 
                         advisory services that consistently support from project conception / planning to execution / operation management.
                         After her second childcare leave, she took a leave of absence and obtained an MBA from Keio Business School (Keio
@@ -82,13 +88,15 @@
         </section>
 
         <section class="l_card__profile">
-            <div class="card_detail">
-                <div class="img_right">
-                    <img class="c_card__img" src="../assets/component/about-card/Advisor/chihiro.jpg" alt="chihiro"/>
-                </div>
+            <div class="l_card_detail">
                 <div class="c_cart__text">
                     <h2>Chihiro Kurose</h2>
-                    <h3>Manager of president's office A2A Town (Cambodia) Co., Ltd</h3>  
+                    <h3>Manager of president's office A2A Town (Cambodia) Co., Ltd</h3>
+                </div>
+                <div class="c_img_right">
+                    <img class="c_card__img right" src="../../public/assets/component/about-card/Advisor/chihiro.jpg" alt="chihiro"/>
+                </div>
+                <div class="c_cart__text">  
                     <p>After graduating from University of Tsukuba, joined a business consulting company after working for a temporary 
                         staffing company. Introduced ERP and promoted organization / business reform projects. Involved in vkirirom 
                         Project since 2015 and is in charge of the corporate office. As female management, she is also the global 
