d6fcc55eead01cc503f5896ff1d2638cfae043a5
metheany <metheany.thorn@gmail..com>
2020-02-10 14:53:41 +0700

    meta-data - test-1

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,11 @@
 <style src="../styles/home.css" scoped></style>
 <template>
   <div id="home-page">
+      <vue-headful
+              :title="title"
+              :description="description"
+              :keywords="keywords"
+      />
       <Slide/>
       <div class="l_section">
         <ContentCard/>
@@ -22,6 +27,13 @@ export default {
     CardProduct,
     ContentCard
   },
-
+    data() {
+        return {
+            title: 'Home',
+            description: 'A2A Digital provides you the best solution for AI Developmentsystem development, enterprise development, digital marketing & film, graphic design, or other functionality, we have the services to help you build sophisticated applications.',
+            site_name: 'A2A Digital',
+            keywords: 'a2a digital, cambodia, AI development, IOT development service, Cloud Computing, RPA install support, Blockchain development, PWA development, VR/AR development, Odoo ERP customize, Digital Marketing, Cyber Security, web development, app development, ux ui'
+        }
+    }
 }
 </script>
