175e6b23754c560b8ea3ba1836d2773c6b7c2343
metheany <metheany.thorn@gmail..com>
2020-02-10 13:17:57 +0700

    vue-headful test-1

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,27 +1,39 @@
 <style src="../styles/home.css" scoped></style>
 <template>
-  <div id="home-page">
-      <Slide/>
-      <div class="l_section">
-        <ContentCard/>
-        <CardProduct/>
-      </div>
-  </div>
+    <div id="home-page">
+        <vue-headful
+                :title="title"
+                :description="description"
+                :keywords="keywords"
+        />
+        <Slide/>
+        <div class="l_section">
+            <ContentCard/>
+            <CardProduct/>
+        </div>
+    </div>
 </template>
 
 <script>
 
-import Slide from '../components/slide/slide.vue'
-import CardProduct from '../components/card-product/card-product.vue'
-import ContentCard from '../components/content-card/content-card.vue'
+    import Slide from '../components/slide/slide.vue'
+    import CardProduct from '../components/card-product/card-product.vue'
+    import ContentCard from '../components/content-card/content-card.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    Slide,
-    CardProduct,
-    ContentCard
-  },
-
-}
+    export default {
+        name: 'Home',
+        components: {
+            Slide,
+            CardProduct,
+            ContentCard
+        },
+        data() {
+            return {
+                title: 'Home',
+                description: 'A2A Digital provides you the best solution for AI Developmentsystem development, enterprise development, digital marketing & film, graphic design, or other functionality, we have the services to help you build sophisticated applications.',
+                site_name: 'A2A Digital',
+                keywords: 'a2a digital, cambodia, AI development, IOT development service, Cloud Computing, RPA install support, Blockchain development, PWA development, VR/AR development, Odoo ERP customize, Digital Marketing, Cyber Security, web development, app development, ux ui'
+            }
+        }
+    }
 </script>
