790b8dfed706b1fe723f4571fe6673d42061bb29
metheany <metheany.thorn@gmail..com>
2020-02-12 17:03:09 +0700

    about-page updated v1

---
+++ src/components/about-overview/about-overview.vue
@@ -0,0 +1,9 @@
+<template src="./about-overview.html"></template>
+
+<script>
+    export default {
+        name: "about-overview"
+    }
+</script>
+
+<style src="./about-overview.css" scoped></style>
