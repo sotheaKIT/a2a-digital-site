96181e98e8de34cdb666dd8c721af6228c01d420
metheany <metheany.thorn@gmail..com>
2020-02-13 16:51:18 +0700

    added tagline-hero to service page

--- src/views/ServiceDetailPage.vue
+++ src/views/ServiceDetailPage.vue
@@ -1,31 +1,34 @@
 <style src="../styles/service-detail.css" scoped></style>
 <template>
-  <div>
-      <div v-for="serviceDetailContent in serviceDetailContents" v-bind:key="serviceDetailContent.id">
-          <vue-headful
-                  :title="serviceDetailContent['meta'][0]['title'] + ' | A2A Digital'"
-                  :description="serviceDetailContent['meta'][0]['description']"
-                  :keywords="serviceDetailContent['meta'][0]['keywords']"
-          />
-        <div class="l_service__detail__container">
-          <div class="c_jobspage_detail l_padding__left">
-            <div class="c_jobspage_title">
-                <a>Service</a>
-                 <img id="c_icon_service" src="../../public/assets/component/service-detail-page/arrow_right.svg">
-                <a id="c_text">{{serviceDetailContent['header'][0]['title']}}</a>
-            </div>
+    <div id="services-page">
+        <div v-for="serviceDetailContent in serviceDetailContents" v-bind:key="serviceDetailContent.id">
+            <vue-headful
+                    :title="serviceDetailContent['meta'][0]['title'] + ' | A2A Digital'"
+                    :description="serviceDetailContent['meta'][0]['description']"
+                    :keywords="serviceDetailContent['meta'][0]['keywords']"
+            />
+            <tagline-hero
+                    :title="serviceDetailContent['header'][0]['title']"
+                    :short_desc="serviceDetailContent['header'][0]['shortDesc']"
+                    imagePath="service"
+            />
+            <ServiceDetailCard v-if="serviceDetailContent['cards'] != null"
+                               :cards="serviceDetailContent['cards']"></ServiceDetailCard>
+            <ServiceDetailPageComp v-if="serviceDetailContent['header'] != null"
+                                   v-bind:header="serviceDetailContent['header']"></ServiceDetailPageComp>
+            <ServiceTechnologyCard v-if="serviceDetailContent['tools'] != null"
+                                   :tools="serviceDetailContent['tools']"></ServiceTechnologyCard>
+            <ServiceWorkResult v-if="serviceDetailContent['results'] != null"
+                               :results="serviceDetailContent['results']"></ServiceWorkResult>
+            <ServiceManagement v-if="serviceDetailContent['management'] != null"
+                               :management="serviceDetailContent['management']"/>
+            <ServiceComapareComp v-if="serviceDetailContent['compare'] != null"
+                                 :compare="serviceDetailContent['compare']"/>
+            <ServiceAIImportant v-if="serviceDetailContent['business'] != null"
+                                :business="serviceDetailContent['business']"></ServiceAIImportant>
+            <ServiceContactForm></ServiceContactForm>
         </div>
-        </div>
-       <ServiceDetailPageComp v-if="serviceDetailContent['header'] != null" v-bind:header="serviceDetailContent['header']" ></ServiceDetailPageComp>
-        <ServiceDetailCard v-if="serviceDetailContent['cards'] != null" :cards="serviceDetailContent['cards']"></ServiceDetailCard>
-        <ServiceTechnologyCard v-if="serviceDetailContent['tools'] != null" :tools = "serviceDetailContent['tools']"></ServiceTechnologyCard>
-        <ServiceWorkResult v-if="serviceDetailContent['results'] != null" :results = "serviceDetailContent['results']"></ServiceWorkResult>
-        <ServiceManagement v-if="serviceDetailContent['management'] != null" :management = "serviceDetailContent['management']" />
-        <ServiceComapareComp v-if="serviceDetailContent['compare'] != null" :compare = "serviceDetailContent['compare']"/>
-        <ServiceAIImportant v-if="serviceDetailContent['business'] != null" :business = "serviceDetailContent['business']"></ServiceAIImportant>
-        <ServiceContactForm></ServiceContactForm>
-      </div>
-  </div>
+    </div>
 </template>
 
 <script>
@@ -38,10 +41,12 @@
     import ServiceComapareComp from '../components/service-cloud-detail-page/service-cloud-detail-page.vue'
     import ServiceManagement from '../components/service-management/service-management'
     import {mapGetters, mapActions} from 'vuex'
+    import TaglineHero from "../components/tagline-hero/tagline-hero";
 
     export default {
         name: 'ServiceDetailPage',
         components: {
+            TaglineHero,
             ServiceDetailPageComp,
             ServiceDetailCard,
             ServiceTechnologyCard,
