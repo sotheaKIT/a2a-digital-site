d3c33c54605d6d7b591ba21d238fb571abdd6e22
Thaung Monyodam <you@example.com>
2020-02-20 10:56:44 +0700

    progressing bar

--- src/main.js
+++ src/main.js
@@ -18,11 +18,6 @@ const options = {
   color: '#138690',
   failedColor: '#874b4b',
   thickness: '5px',
-  transition: {
-    speed: '0.2s',
-    opacity: '0.6s',
-    termination: 300
-  },
   autoRevert: true,
   location: 'top',
   inverse: false
