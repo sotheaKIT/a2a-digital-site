b055a72c595334c8be0669dcee686982c9b5b20a
metheany <metheany.thorn@gmail..com>
2020-02-22 22:19:14 +0700

    pwa feature: test 4

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new ServiceWorkerWebpackPlugin({
+                entry: path.join(__dirname, 'src/service-worker.js'),
+                filename: 'service-worker.js'
+            })
+        ]
+    }
+}
